ddff32a7ce8d70b314421f4c1b0f8e738d34a026
sebanote <sebasbecerraph@gmail.com>
2023-07-30 18:13:38 -0300

    cambios

--- js/main.js
+++ js/main.js
@@ -16,50 +16,53 @@ if (user === userIngresado) {
         let costoCollar = (2 * medialuna + hebilla + regulador + correa * 0.7)
         let costoCorrea = (correa * 1.38 + mosqueton)
         let costoPretal = (correa * 1.84 + regulador + hebilla + medialuna)
-        let producto = prompt(`Que producto deseas comprar \n
+        let producto = +(prompt(`Que producto deseas comprar \n
                      1- Collar\n
                      2- Correa\n
-                     3- Pretal`)
+                     3- Pretal`))
 
         let precioProductoElegido = +("")
         let precioUnidad = + ("")
 
-        switch (producto) {
-            case "1":
-                precioProductoElegido = costoCollar
-                precioUnidad = (+(costoCollar * 2.8)).toFixed(2)
-                console.log(`El precio por unidad es ${precioUnidad}`)
-                break
-            case "2":
-                precioProductoElegido = costoCorrea
-                precioUnidad = (+(costoCorrea * 2.8)).toFixed(2)
-                console.log(precioUnidad)
-                break
-            case "3":
-                precioProductoElegido = costoPretal
-                precioUnidad = (+(costoPretal * 2.8)).toFixed(2)
-                console.log(precioUnidad)
-                break
-            default:
-                console.log(`ingrese una opcion correcta`)
-                break
-        }
-
-        let cantidad = +(prompt(`Que cantidad deseas?`))
-
-
-        function costoventa(x) {
-            if (cantidad < 10) {
-                costofinal = x * cantidad * 2.8
-            } else {
-                costofinal = x * cantidad * 2
+        if ((producto = 1) || (producto = 2) || (producto = 3)) {
+            switch (producto) {
+                case "1":
+                    precioProductoElegido = costoCollar
+                    precioUnidad = (+(costoCollar * 2.8)).toFixed(2)
+                    console.log(`El precio por unidad es ${precioUnidad}`)
+                    break
+                case "2":
+                    precioProductoElegido = costoCorrea
+                    precioUnidad = (+(costoCorrea * 2.8)).toFixed(2)
+                    console.log(precioUnidad)
+                    break
+                case "3":
+                    precioProductoElegido = costoPretal
+                    precioUnidad = (+(costoPretal * 2.8)).toFixed(2)
+                    console.log(precioUnidad)
+                    break
+                default:
+                    console.log(`ingrese una opcion correcta`)
+                    break
             }
-            return costofinal
+
+            let cantidad = +(prompt(`Que cantidad deseas?`))
+
+            function costoventa(x) {
+                if (cantidad < 10) {
+                    costofinal = x * cantidad * 2.8
+                } else {
+                    costofinal = x * cantidad * 2
+                }
+                return costofinal
+            }
+            let costoventas = (costoventa(precioProductoElegido)).toFixed(2)
+
+            console.log(`El costo total es ${costoventas}`)
+
+        } else {
+            console.log(`ingrese una opcion correcta`)
         }
-        let costoventas = (costoventa(precioProductoElegido)).toFixed(2)
-
-        console.log(`El costo total es ${costoventas}`)
-
     } else {
         console.log(`La contraseña ${passIngresada} es incorrecta`)
     }
