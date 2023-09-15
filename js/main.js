836770dcf3e802f05e4dcb92807d1e79417ef3ff
Sebastian Becerra <sebasbecerraph@gmail.com>
2023-09-15 14:18:41 -0300

    cambios en carrito

--- js/main.js
+++ js/main.js
@@ -1,102 +1,8 @@
-/*
-let cinta = 217.08
-let medialuna = 93.92
-let hebilla = 145.43
-let regulador = 76.06
-let mosqueton = 432
-let argolla = 162
-
-const carrito = []
-
-const productos = [
-    {
-        nombre: "Correa",
-        precio: (cinta * 1.38 + mosqueton).toFixed(2),
-        id: 1,
-        categoria: "perros",
-        stock: 20
-    },
-    {
-        nombre: "Collar",
-        precio: (2 * medialuna + hebilla + regulador + cinta * 0.7).toFixed(2),
-        id: 2,
-        categoria: "perros",
-        stock: 20
-    },
-    {
-        nombre: "Pretal",
-        precio: (cinta * 1.84 + regulador + hebilla + medialuna).toFixed(2),
-        id: 3,
-        categoria: "perros",
-        stock: 20
-    },
-    {
-        nombre: "Pretal Anti Escape",
-        precio: (cinta * 1.66 + argolla + regulador * 3 + hebilla).toFixed(2),
-        id: 4,
-        categoria: "perros"
-    },
-    {
-        nombre: "Collar de Gato",
-        precio: (cinta * 0.31 + hebilla + regulador + medialuna).toFixed(2),
-        id: 5,
-        categoria: "gatos",
-        stock: 20
-    }
-]
-
-let inicio
-let lista = productos.map(producto => `${producto.nombre} ID-${producto.id} \n`)
-
-function comprar(id) {
-    let productoBuscado = productos.find(producto => producto.id === id)
-    let precioProducto = productoBuscado.precio
-    let agregarCarrito = confirm(`El precio del producto es $${precioProducto} \n Deseas agregarlo al carrito?`)
-    if (agregarCarrito === true) {
-        let cantidadProductos = +(prompt(`Que cantidad deseas comprar?`))
-        if (cantidadProductos <= productoBuscado.stock) {
-            for (let i = 0; i < cantidadProductos; i++) {
-                (carrito.push(productoBuscado))
-                --productoBuscado.stock
-            }
-        } else {
-            alert(`No tenemos stock \n Nuestro stock de este producto es ${productoBuscado.stock}`)
-        }
-    }
-}
-
-
-
-do {
-    inicio = +(prompt(`Bienvenidos a Kilu Pet Shop \n \n
-1- Ver productos \n
-2- Comprar \n
-3- Ver carrito \n
-4- Salir`))
-    if (inicio === 1) {
-        alert(lista.join(""))
-    } else if (inicio === 2) {
-        let eleccion = prompt(`Qué producto deseas comprar \n${lista.join("")}`)
-        let idEleccion = parseInt(eleccion)
-        comprar(idEleccion)
-    } else if (inicio === 3) {
-        let resumenCompra = carrito.map(producto => `${producto.nombre} $${producto.precio}`).join("\n")
-        alert(resumenCompra)
-    }
-} while (inicio !== 4) {
-    if (carrito.length < 10) {
-        let totalCompra = carrito.reduce((acumulador, producto) => acumulador + parseFloat(producto.precio), 0).toFixed(2)
-        alert(`Gracias por su compra. El total de su compra es $${totalCompra}`)
-    } else {
-        let totalSinDescuento = carrito.reduce((acumulador, producto) => acumulador + parseFloat(producto.precio), 0)
-        let totalConDescuento = (totalSinDescuento * 0.9).toFixed(2)
-        alert(`Gracias por su compra. El total de su compra es $${totalConDescuento} y ha obtenido un descuento del 10% por la compra de más de 10 artículos`)
-    }
-}
-*/
-
 let carrito = []
 let contenedor = document.getElementById("containerProductos")
+const mostrarFinalizarCompra = document.getElementById("menuFinalizarCompra")
+let h3total = document.getElementById("h3total")
+let total
 
 function principal() {
     
@@ -168,7 +74,8 @@ function principal() {
 
             const botonAgregarCarrito = tarjetaProducto.querySelector(".botonAgregarCarrito");
             botonAgregarCarrito.addEventListener("click", comprar)
-        });
+        })
+        mostrarFinalizarCompra.className = "menuOcultoCompra"
     }
 
     function mostrarCarrito(productos) {
@@ -192,15 +99,27 @@ function principal() {
                     <h3>$${producto.cantidad * producto.precio}</h3>
                     </div>
                     `
+                    
+                    producto.subtotal = +(producto.precio) * producto.cantidad
+                    
+                    total = carrito.reduce((accumulator, item) => accumulator + +(item.subtotal), 0)
             contenedor.appendChild(tarjetaProducto)
-        });
+
+
+            if (productos){
+                
+                mostrarFinalizarCompra.className = "menuVisibleCompra"
+            }
+        })
     }
 
     function mostrarCategoria(mostrarCategoria) {
         const categoriaNueva = productos.filter(producto => producto.categoria === mostrarCategoria)
         mostrarProductos(categoriaNueva)
+        mostrarFinalizarCompra.className = "menuOcultoCompra"
     }
 
+    
     const productosTodos = document.getElementById("productosTodos")
     productosTodos.addEventListener("click", () => mostrarProductos(productos))
     const productosPerros = document.getElementById("productosPerros")
@@ -228,13 +147,18 @@ function principal() {
             carrito.push(producto)
         }
     
-        console.log(carrito);
+        console.log(carrito)
+
+        h3total.innerText = "total"
     
         localStorage.setItem("carrito", JSON.stringify(carrito));
     }
     
     mostrarProductos(productos)
+
+    
 }
+
 
 
 principal()
